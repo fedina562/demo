2c92696411e5ed9fa0d4a41eb17c9de522ed4b47
fedina562 <rln562@163.com>
2019-10-21 15:05:34 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController 的意思就是 Controller 里面的方法都以 json 格式输出，不用再写什么 jackjson 配置的了
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/hello")
+    public String index() {
+        return "Hello World";
+    }
+}
